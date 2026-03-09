dbaac6e7ece6c59f08ba0393fc0c919fd30a649d
William Howard-Snyder <william122899@gmail.com>
2026-03-09 17:14:17 -0400

    Add run.bash script for GLUE task execution

---
+++ run.bash
@@ -0,0 +1,16 @@
+export GLUE_DIR=$HOME/COS568-DistLM-SP26/glue_data
+export TASK_NAME=RTE
+
+python3 run_glue.py \
+  --model_type bert \
+  --model_name_or_path bert-base-cased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --data_dir $GLUE_DIR/$TASK_NAME \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 64 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 1 \
+  --output_dir $HOME/$TASK_NAME/ \
+  --overwrite_output_dir
