dd0434d97d473df9723a9b532c5e8edafa17c519
William Howard-Snyder <william122899@gmail.com>
2026-03-09 21:23:20 -0400

    Enhance run.bash with distributed training options
    Added distributed training parameters for GLUE task.

--- run.bash
+++ run.bash
@@ -1,7 +1,12 @@
 export GLUE_DIR=$HOME/COS568-DistLM-SP26/glue_data
 export TASK_NAME=RTE
 
-python3 run_glue.py \
+rank=$1
+ip_prefix=10.10.1
+master_suffix=2
+master_port=29500
+
+python3 task2a/run_glue.py \
   --model_type bert \
   --model_name_or_path bert-base-cased \
   --task_name $TASK_NAME \
@@ -13,4 +18,8 @@ python3 run_glue.py \
   --learning_rate 2e-5 \
   --num_train_epochs 1 \
   --output_dir $HOME/$TASK_NAME/ \
-  --overwrite_output_dir
+  --overwrite_output_dir \
+  --master_ip $ip_prefix"."$master_suffix \
+  --master_port $port \
+  --world_size 4 \
+  --local_rank $rank
